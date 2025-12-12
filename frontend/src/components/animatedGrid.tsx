54479edfdea54d66e9ea12c81f44770663923e3a
DESKTOP-1MCHIFA\Acer <razowanamimi@gmail.com>
2025-12-13 00:43:51 +0600

    Use carvariants

--- frontend/src/components/animatedGrid.tsx
+++ frontend/src/components/animatedGrid.tsx
@@ -16,9 +16,10 @@ const cardVariants = {
   show: {
     opacity: 1,
     y: 0,
-    transition: { duration: 0.5, ease: [0.16, 1, 0.3, 1] }, // FIX HERE
+    transition: { duration: 0.5 }, // uses default ease
   },
 };
+
 
 export default function AnimatedGrid({
   children,
